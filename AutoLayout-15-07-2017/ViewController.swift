ed06ed7ff6a16efe3127310a592c85111703530d
SoengSaravit <soengsaravit@gmail.com>
2017-08-15 23:18:01 +0700

    Initial Commit

--- AutoLayout-15-07-2017/ViewController.swift
+++ AutoLayout-15-07-2017/ViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var questions = ["What’s the name of this city in the United States with a bell in the near middle of the city?",
+                     "What is the name of the spaceship first landed on the moon?",
+                     "What does 'umbros' mean in Latin? Translate it into Sentinelese.",
+                     "What’s the first photo printed from a camera in the world?",
+                     "When was the first email ever sent in the world?",
+                     "… and what was that email about?"]
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func nextQuestionClicked(_ sender: UIButton) {
+        index += 1
+        if index >= questions.count {
+            index = 0
+        }
+        self.questionLabel.text = questions[index]
     }
 
 
